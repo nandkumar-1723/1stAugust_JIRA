b87c80223d7dffe0e25affc41f3fffcb6ca9f02f
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-12-07 20:31:40 +0530

    1st august batch 07/12/2023

---
+++ src/test/java/ReadJsons/ReadCreatedUserStory.java
@@ -0,0 +1,21 @@
+package ReadJsons;
+
+import org.json.simple.parser.*;
+
+import java.io.*;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class ReadCreatedUserStory {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/createUserStory.json");
+        JSONParser js = new JSONParser();
+        String requestBody = js.parse(fr).toString();
+        System.out.println(requestBody);
+
+    }
+
+}
---
+++ src/test/java/ReadJsons/ReadUpdatedUserStory.java
@@ -0,0 +1,19 @@
+package ReadJsons;
+
+import org.json.simple.parser.*;
+
+import java.io.*;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class ReadUpdatedUserStory {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/updateUserStory.json");
+        JSONParser jp = new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+        System.out.println(requestBody);
+    }
+}
