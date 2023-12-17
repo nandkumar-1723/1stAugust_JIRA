23064638764795d055679237ea34f78c9a518638
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-12-17 20:48:40 +0530

    1st august batch 10/12/2023

--- src/test/java/TestCases/CRUD_Bug.java
+++ src/test/java/TestCases/CRUD_Bug.java
@@ -3,6 +3,7 @@ package TestCases;
 import io.restassured.*;
 import io.restassured.http.*;
 import io.restassured.response.*;
+import org.json.*;
 import org.json.simple.parser.*;
 import org.testng.*;
 import org.testng.annotations.*;
@@ -15,7 +16,10 @@ import java.io.*;
  */
 public class CRUD_Bug {
 
-    @Test
+    private String cookieValue;
+    private String issueID;
+
+    @Test(priority = 1)
     public void loginJira() throws Exception {
 
         FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/loginJira.json");
@@ -25,9 +29,81 @@ public class CRUD_Bug {
         Response response = RestAssured.given().baseUri("http://localhost:9009").body(requestBody)
                 .contentType(ContentType.JSON).when().post("/rest/auth/1/session")
                 .then().log().all().extract().response();
+        Assert.assertEquals(response.statusCode(),200);
+
+        JSONObject js = new JSONObject(response.asString());
+         cookieValue = "JSESSIONID="+js.getJSONObject("session").get("value").toString();
+
+    }
+    @Test(priority = 2)
+    public void createBug() throws IOException, ParseException {
+
+    FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/Bug.json");
+    JSONParser jp =new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").body(requestBody)
+                .contentType(ContentType.JSON).header("Cookie", cookieValue)
+                .when().post("/rest/api/2/issue").then().log().all().extract().response();
+
 
         Assert.assertEquals(response.statusCode(),201);
 
+        JSONObject js = new JSONObject(response.asString());
+        issueID =  js.get("key").toString();
+        System.out.println(issueID);
+
+    }
+
+    @Test(priority = 3)
+    public void getBug(){
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").contentType(ContentType.JSON)
+                .header("Cookie", cookieValue).when().get("/rest/api/2/issue/"+issueID+"")
+                .then().log().all().extract().response();
+
+        Assert.assertEquals(response.statusCode(),200);
+
+    }
+
+    @Test(priority = 4)
+    public void updateBug() throws IOException, ParseException {
+
+        FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/Bug.json");
+        JSONParser jp = new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+        JSONObject js = new JSONObject(requestBody);
+        js.getJSONObject("fields").put("summary","updating the bug on 17th december");
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").body(js.toString())
+                .contentType(ContentType.JSON).header("Cookie", cookieValue)
+                .when().put("/rest/api/2/issue/"+issueID+"").then().log().all().extract().response();
+
+        Assert.assertEquals(response.statusCode(),204);
+
+    }
+
+    @Test(priority = 5)
+    public void getUpdatedBug(){
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").contentType(ContentType.JSON)
+                .header("Cookie", cookieValue).when().get("/rest/api/2/issue/"+issueID+"")
+                .then().log().all().extract().response();
+
+        Assert.assertEquals(response.statusCode(),200);
+
+    }
+
+    @Test(priority = 6)
+    public void deleteBug(){
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").contentType(ContentType.JSON)
+                .header("Cookie", cookieValue).when().delete("/rest/api/2/issue/"+issueID+"")
+                .then().log().all().extract().response();
+
+        Assert.assertEquals(response.statusCode(),204);
 
     }
 
---
+++ src/test/java/TestCases/CRUD_Task.java
@@ -0,0 +1,11 @@
+package TestCases;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class CRUD_Task {
+
+    //Assignment....................................................
+
+    //Bug and task screenshots......................(ED- 18/12/2023)
+}
