663eefd15b9e0e7b488611562458b254c57e608c
nandkumar-1723 <127601507+nandkumar-1723@users.noreply.github.com>
2023-12-20 18:53:00 +0530

    1st august batch 10/12/2023

---
+++ src/test/java/TestCases/Param_Query.java
@@ -0,0 +1,67 @@
+package TestCases;
+
+import io.restassured.*;
+import io.restassured.http.*;
+import io.restassured.response.*;
+import org.json.*;
+import org.json.simple.parser.*;
+import org.testng.*;
+import org.testng.annotations.*;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Nandkumar Babar
+ */
+public class Param_Query {
+    private String cookieValue;
+    private String issueID;
+
+    @Test(priority = 1)
+    public void loginJira() throws Exception {
+
+        FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/loginJira.json");
+        JSONParser jp = new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").body(requestBody)
+                .contentType(ContentType.JSON).when().post("/rest/auth/1/session")
+                .then().log().all().extract().response();
+        Assert.assertEquals(response.statusCode(),200);
+
+        JSONObject js = new JSONObject(response.asString());
+        cookieValue = "JSESSIONID="+js.getJSONObject("session").get("value").toString();
+
+    }
+    @Test(priority = 2)
+    public void createBug() throws IOException, ParseException {
+
+        FileReader fr = new FileReader("/home/nandkumar/Videos/1stAugust_JIRA/src/main/java/JsonFiles/Bug.json");
+        JSONParser jp =new JSONParser();
+        String requestBody = jp.parse(fr).toString();
+
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").body(requestBody)
+                .contentType(ContentType.JSON).header("Cookie", cookieValue)
+                .when().post("/rest/api/2/issue").then().log().all().extract().response();
+
+
+        Assert.assertEquals(response.statusCode(),201);
+
+        JSONObject js = new JSONObject(response.asString());
+        issueID =  js.get("key").toString();
+        System.out.println(issueID);
+
+    }
+    @Test(priority = 3)
+    public void getBug(){
+
+        Response response = RestAssured.given().baseUri("http://localhost:9009").contentType(ContentType.JSON)
+                .header("Cookie", cookieValue).queryParam("fields","priority").queryParam("fields","status").when().get("/rest/api/2/issue/"+issueID+"")
+                .then().log().all().extract().response();
+
+        Assert.assertEquals(response.statusCode(),200);
+
+    }
+}
